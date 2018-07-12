fffe357481cd2208236226eeedc5c244e00c7a2b
Carl Johnson <carl@protectamerica.com>
2018-07-12 12:44:44 -0500

    refactored to use Flask instead of CherryPy

--- main.py
+++ main.py
@@ -2,38 +2,34 @@ import json
 import random
 import string
 
-import cherrypy
+from flask import Flask
 import redis
 
 
-@cherrypy.expose
-class Main(object):
-    #TODO: load host, port, and db from confs
-    redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+app = Flask(__name__)
+
+#TODO: load host, port, and db from confs
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+@app.route('/', methods=['POST'])
+def receive_data(data):
+    #TODO: take expiration time from confs
+    filename = self.random_filename()
+    redis_client.set(filename, data, ex=60, nx=True)
     
-    
-    def POST(self, data):
-        #TODO: take expiration time from confs
-        filename = self.random_filename()
-        redis_client.set(filename, data, ex=60, nx=True)
-        
-        #TODO: automatic dumpsing how?
-        return json.dumps({'url': filename})
-    
-    ################################
-    
-    @classmethod
-    def random_filename(cls):
-        return ''.join(random.choice(string.ascii_lowercase) for _ in range(32))
+    #TODO: automatic dumpsing how?
+    return json.dumps({'url': filename})
+
+@app.route('/<id_>', methods=['GET'])
+def divulge_data(id_=None):
+    to_return = app.make_response(redis_client.get(id_))
+    redis_client.expire(id_, 0)
+    return to_return
+
+def random_filename():
+    return ''.join(random.choice(string.ascii_lowercase) for _ in range(32))
 
 
 if __name__=='__main__':
-    conf = {
-        '/': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'application/json')]
-        }
-    }
-    
-    cherrypy.quickstart(Main())
+    app.run()
