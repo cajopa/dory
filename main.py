59411948142dca05df7667e6d7c95477453f90fb
Carl Johnson <carl@protectamerica.com>
2018-07-16 13:18:12 -0500

    refactored to subclass Flask instead of calling and mutating it, and reverted the use of abort

--- main.py
+++ main.py
@@ -4,44 +4,47 @@ import json
 import random
 import string
 
-from flask import Flask, request, Response, abort
+from flask import Flask, request, Response
 import redis
 
 
-app = Flask('dory')
+class DoryApp(Flask):
+    def __init__(self):
+        super(DoryApp, self).__init__('dory')
+        
+        #TODO: load host, port, and db from confs
+        self.redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+        
+        self.add_url_rule('/', 'receive_data', self.receive_data, methods=['POST'])
+        self.add_url_rule('/<id_>', 'divulge_data', self.divulge_data, methods=['GET'])
 
-#TODO: load host, port, and db from confs
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+    def receive_data(self):
+        #TODO: take expiration time from confs
+        while True: #loop handles collisions
+            keyname = self.random_keyname()
+            if self.redis_client.set(keyname, request.get_data(cache=False), ex=60, nx=True):
+                break
+        
+        return Response(
+            response=json.dumps({'url': keyname}),
+            mimetype='application/json'
+        )
 
-
-@app.route('/', methods=['POST'])
-def receive_data():
-    #TODO: take expiration time from confs
-    while True: #loop handles collisions
-        keyname = random_keyname()
-        if redis_client.set(keyname, request.get_data(cache=False), ex=60, nx=True):
-            break
+    def divulge_data(self, id_=None):
+        with self.redis_client.pipeline() as p:
+            p.get(id_)
+            p.delete(id_)
+            to_return = p.execute()[0]
+        
+        if to_return:
+            return self.make_response(to_return)
+        else:
+            return Response(status=404)
     
-    return Response(
-        response=json.dumps({'url': keyname}),
-        mimetype='application/json'
-    )
-
-@app.route('/<id_>', methods=['GET'])
-def divulge_data(id_=None):
-    with redis_client.pipeline() as p:
-        p.get(id_)
-        p.delete(id_)
-        to_return = p.execute()[0]
-    
-    if to_return:
-        return app.make_response(to_return)
-    else:
-        abort(404)
-
-def random_keyname():
-    return ''.join(random.choice(string.ascii_lowercase) for _ in range(32))
+    @classmethod
+    def random_keyname(cls):
+        return ''.join(random.choice(string.ascii_lowercase) for _ in range(32))
 
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5001)
+    DoryApp().run(host='0.0.0.0', port=5001)
