7e82dc774538796bf67e30fc084dff56d3c762ce
Carl Johnson <carl@protectamerica.com>
2018-07-16 12:47:10 -0500

    it works!

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
+#!/usr/bin/env python
+
 import json
 import random
 import string
 
-from flask import Flask
+from flask import Flask, request, Response
 import redis
 
 
-app = Flask(__name__)
+app = Flask('dory')
 
 #TODO: load host, port, and db from confs
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
 @app.route('/', methods=['POST'])
-def receive_data(data):
+def receive_data():
     #TODO: take expiration time from confs
-    filename = self.random_filename()
-    redis_client.set(filename, data, ex=60, nx=True)
+    while True: #loop handles collisions
+        keyname = random_keyname()
+        if redis_client.set(keyname, request.get_data(cache=False), ex=60, nx=True):
+            break
     
-    #TODO: automatic dumpsing how?
-    return json.dumps({'url': filename})
+    return Response(response=json.dumps({'url': keyname}), mimetype='application/json')
 
 @app.route('/<id_>', methods=['GET'])
 def divulge_data(id_=None):
-    to_return = app.make_response(redis_client.get(id_))
-    redis_client.expire(id_, 0)
-    return to_return
+    with redis_client.pipeline() as p:
+        p.get(id_)
+        p.delete(id_)
+        to_return = p.execute()[0]
+    
+    if to_return:
+        return app.make_response(to_return)
+    else:
+        return Response(status=404)
 
-def random_filename():
+def random_keyname():
     return ''.join(random.choice(string.ascii_lowercase) for _ in range(32))
 
 
 if __name__=='__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5001)
